550412b0c3e7e336b4a05480a6b9d8c6ccdcc607
Richard Agyekum <quadjogh95@gmail.com>
2023-02-18 22:02:25 +0000

    When I was having the alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char let;
+	char e;
+	char q;
+
+	e = 'e';
+	q = 'q';
+
+	for (let = 'a'; let <= 'z'; let++)
+		if (let != q && let != e)
+		{
+			putchar(let);
+		}
+	putchar('\n');
+	return (0);
+}

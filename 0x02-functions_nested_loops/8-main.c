4df97f62466895ee5b0ba1041f4f790dd3764683
Richard Agyekum <quadjogh95@gmail.com>
2023-02-22 10:21:08 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* *
+ * main - check the code
+ *
+ * jack_bauer: print every minute in the day
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	jack_bauer();
+
+	return (0);
+}
